f612f40b56c12d8eed2cd159c71b2b4c80a54260
Jonas Libbrecht <jonas.libbrecht@ext.ec.europa.eu>
2024-10-29 00:03:11 +0100

    Move to manifest v3

--- background.js
+++ background.js
@@ -1,17 +1,3 @@
-// Page Action
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {
-              urlMatches: 'zendesk.com/agent'
-            },
-          })
-        ],
-        actions: [ new chrome.declarativeContent.ShowPageAction() ]
-      }
-    ]);
-  });
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Extension installed!");
 });
